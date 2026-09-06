d521fd78307b001fef7aa18844fb341678f83312
sterling566-droid <johnsterlingmensah566@gmail.com>
2026-09-06 00:22:17 +0000

    Change API key and system message in open.py
    Updated API key placeholder and modified system message content.

--- open.py
+++ open.py
@@ -2,13 +2,13 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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"
+    api_key="your openrouter api key"
 )
 
 response = client.chat.completions.create(
     model="openrouter/free",
     messages=[
-      {"role":"system","content":"Your name is Sterling you are Mr.Sterling's personal assistant he is 16yrs and he us a Ghanian Address him as Mr.Sterling.."},
+      {"role":"system","content":"models personality"},
         {"role": "user", "content":"generate a image of a dog"}
     ]
 )
